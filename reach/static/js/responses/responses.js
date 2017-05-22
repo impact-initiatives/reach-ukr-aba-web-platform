60378139a3c1d5cf9c99ba1ad8cb2ffab691f284
denysboiko <denys.bojko@gmail.com>
2017-05-22 11:48:02 +0300

    questionnaire app

--- reach/static/js/responses/responses.js
+++ reach/static/js/responses/responses.js
@@ -1,20 +1,5 @@
 function loadResponses(fields, data) {
 
-    // console.log(data)
-    var features = [];
-
-    data.forEach(function (e, i) {
-
-        var id = i,
-            lat = e['_gpslocation_latitude'],
-            lon = e['_gpslocation_longitude'];
-
-
-        var feature = newFeature(lon, lat, id);
-        features.push(feature);
-
-    });
-
     mapboxgl.accessToken = 'pk.eyJ1IjoiZGVueXNib2lrbyIsImEiOiJjaXpxdzlxMGswMHMzMnFxbzdpYjJoZDN1In0.O3O4iBtTiODWN0C8oGOBwg';
     var map = new mapboxgl.Map({
         container: 'map-container', // container id
@@ -30,25 +15,23 @@ function loadResponses(fields, data) {
 
     map.on('load', function () {
 
+        var features = [];
+        data.forEach(function (e, id) {
+            var lat = e['_gpslocation_latitude'],
+                lon = e['_gpslocation_longitude'];
+            var feature = newFeature(lon, lat, id);
+            features.push(feature);
+        });
+
         var geoData = {
             type: "FeatureCollection",
             features: features
         };
 
-        // console.log(geoData);
-
         map.addSource('responses', {
             type: "geojson",
             data: geoData
         });
-
-
-        /*            if (map.getSource('responses')) /*}
-         /*                map.getSource('responses').setData(/*}
-         /*                    type: "FeatureCollection",*/
-        /*                    features: newFeature()*/
-        /*                });*/
-        /*            }*/
 
 
         if (!map.getLayer("responses")) {
@@ -62,13 +45,20 @@ function loadResponses(fields, data) {
                 'paint': {
                     'circle-radius': 6,
                     'circle-color': 'rgb(238,88,89)',
+
+                    // 'circle-color': {
+                    //     property: 'Sector',
+                    //     type: 'categorical',
+                    //     stops: sectors
+                    // },
+
                     'circle-stroke-color': '#000000',
                     'circle-stroke-width': 1,
                     "circle-opacity": 1
                 }
             });
         }
-    })
+    });
 
     map.on('click', function (e) {
 
@@ -82,22 +72,10 @@ function loadResponses(fields, data) {
 
         if (feature.layer.source == 'responses') {
             var ind = feature.properties['id'];
-            console.log(data[ind]);
-
-
-
             renderTable(fields, data[ind]);
+
             $('#myModal').modal('show');
-            // var popup = new mapboxgl.Popup()
-            // .setLngLat(feature.geometry.coordinates)
-            // .setHTML(
-            //     '<b>Oblast:</b> '
-            //     +feature.properties['ADMIN_1_EN']
-            //     +'<br>'
-            //     +'<b>Full name:</b> '
-            //     +feature.properties['NAME']
-            // )
-            // .addTo(map);
+
         }
 
 
